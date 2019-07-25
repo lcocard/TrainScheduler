c633a0acb65381546867896556561cddeed2ab85
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-07-25 08:01:41 -0400

    create input elements and data validation

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,39 +18,98 @@ firebase.initializeApp(config);
 // Create a variable to reference the database
 var database = firebase.database();
 
-$("#submitTrain").on("click", function (event) {
-    event.preventDefault();
+// Wait until the DOM has been fully parsed
+window.addEventListener("DOMContentLoaded", function () {
+
+    // Get DOM references:
+
     // Get the input values
-    var InputName = $("#InputName").val().trim();
-    var InputName = $("#InputName").val().trim();
-    var InputName = $("#InputName").val().trim();
-    var bidderPrice = parseInt($("#bidder-price").val().trim());
+    var theForm = document.querySelector("#frmTrainInput");
+    var InputTrainName = document.querySelector("#InputTrainName");
+    var InputDestination = document.querySelector("#InputDestination");
+    var InputFirstTrain = document.querySelector("#InputFirstTrain");
+    var InputFrequency = document.querySelector("#InputFrequency");
+    var theSubmitButton = document.getElementById("#submitTrain");
 
-    // Log the Bidder and Price (Even if not the highest)
-    console.log(bidderName);
-    console.log(bidderPrice);
 
-    if (bidderPrice > highPrice) {
 
-        // Alert
-        alert("You are now the highest bidder.");
+    // Because forms can be submitted via submit buttons but also from pressing ENTER,
+    // we need to make sure the form has gone through custom validation before submit
+    // actually happens. So, we'll validate on the submit button's click event as well
+    // as the form submit.
 
-        // Save the new price in Firebase. This will cause our "value" callback above to fire and update
-        // the UI.
+
+    theForm.addEventListener("submit", validate);
+    submitTrain.addEventListener("click", validate);
+
+    function validate(evt) {
+
+        alert("hello reset");
+
+        // Reset the validity of the phone number
+        InputTrainName.setCustomValidity("");
+        InputDestination.setCustomValidity("");
+        InputFirstTrain.setCustomValidity("");
+        InputFrequency.setCustomValidity("");
+
+        // Check to see if the form is INVALID for any reason
+        if (!theForm.checkValidity()) {
+
+            // Check to see if it is the Train Name that is the problem:
+            if (!InputTrainName.validity.valid) {
+                // Set up your own custom error message from whatever source you like
+                // Here, it's just hard coded:
+                InputTrainName.setCustomValidity("Please enter a valid train name!");
+            }
+
+            // Check to see if it is the Destination that is the problem:
+            if (!InputDestination.validity.valid) {
+                // Set up your own custom error message from whatever source you like
+                // Here, it's just hard coded:
+                InputDestination.setCustomValidity("Please enter a valid destination!");
+            }
+
+            // Check to see if it is the First Train Time that is the problem:
+            if (!InputFirstTrain.validity.valid) {
+                // Set up your own custom error message from whatever source you like
+                // Here, it's just hard coded:
+                InputFirstTrain.setCustomValidity("Please enter a valid start time!");
+            }
+
+            // Check to see if it is the Frequency that is the problem:
+            if (!InputFrequency.validity.valid) {
+                // Set up your own custom error message from whatever source you like
+                // Here, it's just hard coded:
+                InputFrequency.setCustomValidity("Please enter a valid frequency time!");
+            }
+
+        }
+
+        var valInputTrainName = $("#InputTrainName").val();
+        var valInputDestination = $("#InputDestination").val();
+        var valInputFirstTrain = $("#InputFirstTrain").val();
+        var valInputFrequency = $("#InputFrequency").val();
+
+        // Log 
+        console.log("InputTrainName = " + valInputTrainName);
+        console.log("InputDestination = " + valInputDestination);
+        console.log("InputFirstTrain = " + valInputFirstTrain);
+        console.log("InputFrequency = " + valInputFrequency);
+
         database.ref().set({
-            highBidder: bidderName,
-            highPrice: bidderPrice
+            TrainName: valInputTrainName,
+            Destination: valInputDestination,
+            FirstTrain: valInputFirstTrain,
+            Frequency: valInputFrequency
+
         });
 
-        // Log the new High Price
-        console.log("New High Price!");
-        console.log(bidderName);
-        console.log(bidderPrice);
-    }
 
-    else {
+        $("#InputTrainName").val("");
+        $("#InputDestination").val("");
+        $("#InputFirstTrain").val("");
+        $("#InputFrequency").val("");
 
-        // Alert
-        alert("Sorry that bid is too low. Try again.");
     }
 });
+
